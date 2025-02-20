2531670fb240b4ac69d955f79b4c1944e983a172
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-02-21 04:52:57 +0500

    commit

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+
 
 const Users = () => {
   const [users, setUsers] = useState([]);
