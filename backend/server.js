a373b5bd3652018b6d36bc3847e528dfa7a52e68
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-02-21 05:31:50 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 const auth = require("./routes/auth");
 const analyticsRouter = require("./routes/analytics");
-
+const path=require("path");
 const app = express();
+
+const __dirname=path.resolve()
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,6 +27,14 @@ app.use("/api/reviews", require("./routes/reviews"));
 app.use("/api/analytics", analyticsRouter);
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", auth);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname,  "../frontend", "dist", "index.html"))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
